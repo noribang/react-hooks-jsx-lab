05cb0cc27ca71bd7a1eff3678b06c5cc67bdddd1
hoonkim <hoonk9387@gmail.com>
2022-05-29 00:11:34 -0500

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  const altString = "I made this"
+
+  return (<div id="about">
+            <h2>About Me</h2>
+            <p>Learning React!</p>
+            <img src={image} alt={altString}></img>
+          </div>
+  );
 }
 
 export default About;
